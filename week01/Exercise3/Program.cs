273d3636a7f202c2211b0f5e1c26ffd3e1ec2684
RenataBrunelle <rbamenezes@gmail.com>
2026-03-07 19:58:08 -0300

    week_1_exercise_3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int guess = 0;
+        Random randomGenerator = new Random();
+        int number_magic = randomGenerator.Next(1, 101);
+
+        do
+        {
+            Console.WriteLine("Informe seu palpite: ");
+            string current_guess = Console.ReadLine();
+            guess = int.Parse(current_guess);
+            if (guess < number_magic)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > number_magic)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (guess != number_magic);
+
+        Console.WriteLine("You guessed it!");
     }
 }
