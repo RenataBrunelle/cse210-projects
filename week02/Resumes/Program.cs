768328885910b3734df6c7fd2952e0a816d4d96a
RenataBrunelle <rbamenezes@gmail.com>
2026-03-09 23:35:29 -0300

    week_2_exercise_1

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,31 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        //Criar o objeto (usando new Job()).
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 1999;
+        job1._endYear = 2020;
+
+        //Cria a segubda instância
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Microsoft";
+        job2._startYear = 1995;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Renata";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
+
+//No C#, para usar uma classe, você precisa primeiro declarar ela (no arquivo Job.cs) e depois instanciar 
+//(criar um objeto dela no Program.cs).
