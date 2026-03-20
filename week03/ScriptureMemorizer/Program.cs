4ae364f2347c6c1e866a0b5f554610661190fafa
RenataBrunelle <rbamenezes@gmail.com>
2026-03-19 21:12:36 -0300

    week3_Scripture_Memorizer

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,40 @@
 using System;
-
+//If the user types "show", it displays the entire script text.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference _reference = new Reference("Provérbios", 3, 5);
+        string text = "Trust in the Lord with all your heart and lean not on your own understanding.";
+        Scripture scripture = new Scripture(_reference, text);
+
+        string userInput = "";
+
+        while (userInput.ToLower() != "quit" && !scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine();
+
+            Console.WriteLine("Press Enter to hide or type 'quit' if you want to exit, finally, if you want to show all the text, type 'show': ");
+            userInput = Console.ReadLine().ToLower();
+
+            if (userInput == "quit")
+            {
+                break;
+            }
+            else if (userInput == "show")
+            {
+                scripture.ShowAllWords();
+            }
+            else
+            {
+                scripture.HiderRandomWords(3);
+            }
+        }
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+        Console.WriteLine("Program closed");
     }
 }
+
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+public class Scripture
+{
+    private Reference _reference;
+    List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+        string[] splitWords = text.Split(' ');
+        foreach (string wordText in splitWords)
+        {
+            Word newObjectWord = new Word(wordText);
+            _words.Add(newObjectWord);
+        }
+    }
+
+    public void HiderRandomWords(int numberToHide)
+    {
+        Random generator = new Random();
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int indexDrawn = generator.Next(_words.Count);
+            _words[indexDrawn].Hide();
+        }
+    }
+
+    public string GetDisplayText()
+    {
+
+        string fullText = _reference.GetDisplayText();
+        fullText += " ";
+
+        foreach (Word word in _words)
+        {
+            string text = word.GetDisplayText();
+            fullText += text + " ";
+        }
+        return fullText.Trim();
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (word.IsHidden() == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void ShowAllWords()
+    {
+        foreach (Word word in _words)
+        {
+            word.Show(); 
+        }
+    }
+
+}
