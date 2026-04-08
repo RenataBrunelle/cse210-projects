59128790b8dcae895096f294bf0b5f91141fa8a0
RenataBrunelle <rbamenezes@gmail.com>
2026-04-07 21:11:35 -0300

    week6-shape

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Pink", 3);
+        shapes.Add(s1);
+
+        Circle s2 = new Circle("Yellow", 4);
+        shapes.Add(s2);
+
+        Rectangle s3 = new Rectangle("Blue", 4,3);
+        shapes.Add(s3);
+        
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape  has an area of {area}.");
+        }
     }
 }
